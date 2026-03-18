ff3d0566fe3d977c761fba2edf5a8e731ee10cd8
Ian <chunying.huang@leishan.com.tw>
2026-03-18 13:44:14 +0800

    refactor: Migrate Tailwind CSS integration from PostCSS to the new Vite plugin.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
 
 export default defineConfig(() => {
@@ -12,6 +13,7 @@ export default defineConfig(() => {
     },
     plugins: [
       react(),
+      tailwindcss(),
       viteCommonjs({
         include: ['mathjax-full']
       }),
